335df2022ea8b09d90c9ea36dfb6456e56afec86
kiy <fcfdsa>
2021-09-21 20:13:24 +0700

    Test User v2

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
 const app = require('../index')
 
-describe('GET /user', () => {
+describe('GET /', () => {
     it('responds with "No user in the system"', (done) => {
-        request(app).get('/').expect('No user in the system', done)
+        request(app).get('/user').expect('No user in the system', done)
     })
 })
